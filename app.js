9a3c9de4359ad5ef1729d546a20a84c607474a70
Carlos Mateos <carlosmateos829@outlook.com>
2022-10-14 17:52:57 -0500

    easy design

--- app.js
+++ app.js
@@ -5,12 +5,6 @@ let addNum = (number) => document.getElementById("inputScreen").value += number;
 
 let deleteNums = () => document.getElementById("inputScreen").value = null;
 
-
-let deleteOne = () => {
-    let inputValue = inputScreen.value;
-    inputScreen.value = inputValue.substring(0, inputValue.length-1);
-}
-
 let operators = (operator) => {
     inputScreen.value += operator;
 }
@@ -18,3 +12,4 @@ let operators = (operator) => {
 let resultado = () => {
     inputScreen.value = eval(inputScreen.value);    
 }
+
