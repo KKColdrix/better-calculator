96bb1b8df4f30814b98eb94a9a0983f82a981ca5
Carlos Mateos <carlosmateos829@outlook.com>
2022-10-17 00:21:19 -0500

    add more buttons

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var inputScreen = document.getElementById("inputScreen"), lastOp;
+var inputScr = document.getElementById("inputScreen");
 
-let addNum = (number) => document.getElementById("inputScreen").value += number;
+let addNum = (number) => inputScr.value += number;
 
+let addPunto = () => inputScr.value += ".";
 
 let deleteNums = () => document.getElementById("inputScreen").value = null;
 
-let operators = (operator) => {
-    inputScreen.value += operator;
-}
+let operators = (operator) => inputScr.value += operator;
 
-let resultado = () => {
-    inputScreen.value = eval(inputScreen.value);    
-}
+let resultado = () => inputScr.value = eval(inputScr.value);
 
+let potencia = () => {
+    let valor = parseInt(inputScr.value) * parseInt(inputScr.value);
+    inputScr.value = valor
+};
